c1d01fbd05e5cda7651a407422e0f07b86494c6d
muhammad <m.dyhaz@gmail.com>
2023-03-14 01:20:15 +0700

    feat: add login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,7 +9,7 @@
 <body>
 <div class="container mt-5">
     <h1>Login</h1>
-    <form action="{{ url('/api/login') }}" method="POST">
+    <form id="login-form">
         @csrf
         <div class="form-group">
             <label for="email">Email address</label>
@@ -21,8 +21,32 @@
         </div>
         <button type="submit" class="btn btn-primary">Login</button>
     </form>
+    <div id="message"></div>
 </div>
 
+<script src="{{ asset('js/jquery.min.js') }}"></script>
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script>
+    $(document).ready(function() {
+        $('#login-form').on('submit', function(event) {
+            event.preventDefault();
+            var formData = $(this).serialize();
+            $.ajax({
+                url: "{{ url('/api/login') }}",
+                type: "POST",
+                data: formData,
+                dataType: "json",
+                success: function(data) {
+                    $('#message').html('<div class="alert alert-success">'+data.message+'</div>');
+                    window.location.href = "{{ url('/') }}";
+                },
+                error: function(xhr, status, error) {
+                    var err = eval("(" + xhr.responseText + ")");
+                    $('#message').html('<div class="alert alert-danger">'+err.error+'</div>');
+                }
+            });
+        });
+    });
+</script>
 </body>
 </html>
