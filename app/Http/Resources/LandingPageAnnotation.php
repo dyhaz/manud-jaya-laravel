05f0c30f4e4094a74405433c6dee9c91ec750b0f
muhammad <m.dyhaz@gmail.com>
2023-04-15 09:32:58 +0700

    fix: fix landing page controller

--- app/Http/Resources/LandingPageAnnotation.php
+++ app/Http/Resources/LandingPageAnnotation.php
@@ -14,7 +14,31 @@ namespace App\Http\Resources;
  *     @OA\Property(property="subtitle", type="string"),
  *     @OA\Property(property="visi", type="string"),
  *     @OA\Property(property="misi", type="string"),
- *     @OA\Property(property="about_manud_jaya", type="string")
+ *     @OA\Property(property="about_manud_jaya", type="string"),
+ *     @OA\Property(
+ *         property="aparat_desa",
+ *         type="array",
+ *         @OA\Items(
+ *             type="object",
+ *             required={
+ *                 "photo",
+ *                 "name",
+ *                 "position"
+ *             },
+ *             @OA\Property(
+ *                 property="photo",
+ *                 type="string"
+ *             ),
+ *             @OA\Property(
+ *                 property="name",
+ *                 type="string"
+ *             ),
+ *             @OA\Property(
+ *                 property="position",
+ *                 type="string"
+ *             )
+ *         )
+ *     )
  * )
  *
  * @OA\Schema(
